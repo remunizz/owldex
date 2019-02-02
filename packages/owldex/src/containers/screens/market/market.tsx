6b393beb82aab1ac042dfa6122e3b04df106409e
Renan Muniz <renan@renanmuniz.com>
2019-02-02 15:04:39 +0100

    style(market): update borders, transitions and text

--- packages/owldex/src/containers/screens/market/market.tsx
+++ packages/owldex/src/containers/screens/market/market.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { FunctionComponent } from "react";
 import { fetchSet } from "../../../actions/entities";
 import { Card } from "magic-api-http";
 import { CardComponent } from "../../../components/card";
@@ -9,6 +9,7 @@ import { RouteComponentProps } from "react-router";
 import { parse, stringify } from "query-string";
 import { Link } from "react-router-dom";
 import { CardDetails } from "../../../components/card-details";
+import { Dialog } from "../../../components/dialog";
 
 const cx = classnames.bind(styles);
 
@@ -20,6 +21,48 @@ interface MarketProps extends RouteComponentProps {
   detailCard: Card[];
 }
 
+interface SetSelectorProps {
+  onSelect: () => void;
+}
+
+const SetSelector: React.FunctionComponent<SetSelectorProps> = ({
+  onSelect
+}) => (
+  <div>
+    <Dialog label="Fetch a card set">
+      <button onClick={onSelect} className={cx("btn")}>
+        EMA
+      </button>
+    </Dialog>
+  </div>
+);
+
+interface CardListProps {
+  showDetails: boolean | undefined;
+  cards: Card[];
+}
+
+const CardList: FunctionComponent<CardListProps> = ({ showDetails, cards }) => (
+  <div className={cx("card-list", { "fixed-pos": showDetails })}>
+    {cards.map(card => (
+      <div className={cx("card-container")} key={card.id}>
+        <Link
+          to={{
+            search: stringify({
+              detailCardName: card.name,
+              modal: true,
+              detailCardId: card.id
+            })
+          }}
+          className={cx("anchor")}
+        >
+          <CardComponent {...card} />
+        </Link>
+      </div>
+    ))}
+  </div>
+);
+
 export const MarketComponent: React.FunctionComponent<MarketProps> = ({
   actions,
   cards,
@@ -28,38 +71,20 @@ export const MarketComponent: React.FunctionComponent<MarketProps> = ({
 }) => {
   const { search } = location;
   const { modal } = parse(search);
-  const showDetails = modal && detailCard && detailCard[0];
+  const showDetails =
+    modal === "true" && detailCard !== undefined && detailCard[0] !== undefined;
 
   return (
     <React.Fragment>
-      <div className={cx("card-wrapper")}>
-        {cards &&
-          cards.length > 0 && (
-            <div className={cx("card-list", { "fixed-pos": showDetails })}>
-              {cards.map(card => (
-                <div className={cx("card-container")} key={card.id}>
-                  <Link
-                    to={{
-                      search: stringify({
-                        detailCardName: card.name,
-                        modal: true,
-                        detailCardId: card.id
-                      })
-                    }}
-                    className={cx("anchor")}
-                  >
-                    <CardComponent {...card} />
-                  </Link>
-                </div>
-              ))}
-            </div>
-          )}
-        {cards.length === 0 && (
-          <button onClick={e => actions.fetchSet("ema")} className={cx("btn")}>
-            Download
-          </button>
+      {cards &&
+        cards.length > 0 && (
+          <div className={cx("card-wrapper")}>
+            <CardList cards={cards} showDetails={showDetails} />
+          </div>
         )}
-      </div>
+      {cards.length === 0 && (
+        <SetSelector onSelect={() => actions.fetchSet("ema")} />
+      )}
       {showDetails && <CardDetails cardData={detailCard[0]} />}
     </React.Fragment>
   );
